42f0ab4cea4fc658ad627504670f08d9ae3c2d45
zypherscript <kitti.kthp@gmail.com>
2023-06-28 14:25:09 +0700

    fix client app

--- api-client/src/main/java/com/example/apiclient/SecurityConfig.java
+++ api-client/src/main/java/com/example/apiclient/SecurityConfig.java
@@ -1,26 +1,26 @@
-//package com.example.apiclient;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//  @Bean
-//  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    http
-//        .authorizeHttpRequests(authorizeRequests ->
-//            authorizeRequests.anyRequest().authenticated()
-//        )
-//        .oauth2Login(oauth2Login ->
-//            oauth2Login.loginPage("/oauth2/authorization/articles-client-oidc"))
-//        .oauth2Client(withDefaults());
-//    return http.build();
-//  }
-//}
+package com.example.apiclient;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(authorizeRequests ->
+            authorizeRequests.anyRequest().authenticated()
+        )
+        .oauth2Login(oauth2Login ->
+            oauth2Login.loginPage("/oauth2/authorization/articles-client-oidc"))
+        .oauth2Client(withDefaults());
+    return http.build();
+  }
+}
