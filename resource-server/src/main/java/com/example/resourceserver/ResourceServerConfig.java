63009927f50213af8a2916587af1e12fa488222f
zypherscript <kitti.kthp@gmail.com>
2023-06-28 14:05:18 +0700

    fix rsrc server

--- resource-server/src/main/java/com/example/resourceserver/ResourceServerConfig.java
+++ resource-server/src/main/java/com/example/resourceserver/ResourceServerConfig.java
@@ -1,17 +1,20 @@
 package com.example.resourceserver;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
 public class ResourceServerConfig {
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .csrf(AbstractHttpConfigurer::disable)
+        .securityMatcher("/messages/**")
         .authorizeHttpRequests(authz -> authz
             .requestMatchers("/articles/**")
             .hasAuthority("SCOPE_articles.read")
